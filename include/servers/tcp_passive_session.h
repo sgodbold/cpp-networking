6352d216ae81f1cbffcda9f14f637ff4311d1d38
Steven Godbold <steven.godbold@gmail.com>
2016-05-30 18:44:15 -0400

    added passive server that only accepts connections

---
+++ include/servers/tcp_passive_session.h
@@ -0,0 +1,27 @@
+#ifndef CPP_NETWORKING_TCP_PASSIVE_SESSION_H
+#define CPP_NETWORKING_TCP_PASSIVE_SESSION_H
+
+#include "servers/tcp_base_session.h"
+
+#include <vector>
+
+#include "boost_definitions.h"
+#include <boost/asio.hpp>
+
+namespace net {
+
+class Tcp_Passive_Session : public Tcp_Base_Session
+{
+public:
+    Tcp_Passive_Session(boost::asio::ip::tcp::socket socket);
+
+protected:
+    void do_read_work(std::shared_ptr<std::vector<char>> data_ptr,
+                      boost::system::error_code) override;
+
+    void do_write_work(boost::system::error_code, std::size_t length) override;
+}; // Tcp_Passive_Session
+
+} // net
+
+#endif
---
+++ src/servers/tcp_passive_session.cpp
@@ -0,0 +1,29 @@
+#include "servers/tcp_passive_session.h"
+#include "servers/tcp_base_session.h"
+
+#include <boost/asio.hpp>
+
+#include <iostream>
+#include <memory>
+#include <vector>
+
+using boost::asio::const_buffer;
+using boost::asio::streambuf;
+using boost::system::error_code;
+
+using net::Tcp_Passive_Session;
+
+using std::shared_ptr; using std::make_shared;
+using std::cout; using std::endl;
+
+Tcp_Passive_Session::Tcp_Passive_Session(boost::asio::ip::tcp::socket socket)
+    : net::Tcp_Base_Session(std::move(socket))
+{
+    cout << " | Passive session started" << endl;
+}
+
+void Tcp_Passive_Session::do_read_work(shared_ptr<std::vector<char>>, error_code)
+{}
+
+void Tcp_Passive_Session::do_write_work(error_code, size_t)
+{}
